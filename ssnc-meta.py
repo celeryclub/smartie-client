3282d36c38e0b210b2ee29abe56d423ede2f6b01
Stephen Davis <stephendavis89@gmail.com>
2015-04-03 14:56:33 -0400

    Restructure Python script to execute correctly

--- ssnc-meta.py
+++ ssnc-meta.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import argparse, re #, base64
+import argparse, re
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--format', required=True)
@@ -8,67 +8,51 @@ parser.add_argument('-e', '--endscreen')
 parser.add_argument('fifo')
 args = parser.parse_args()
 
-# print args.format
-# print args.endscreen
-# print args.fifo
-
-metadata_in_progress=False
-data_mode=False
+metadata = {}
+reading_header = False
+reading_data = False
+next_data_bucket = None
 
 with open(args.fifo) as f:
   for line in f:
-    tag_match = re.match('<type>(\w+)<\/type><code>(\w+)<\/code><length>(\d+)<\/length>', line, flags=re.IGNORECASE)
-    data_match = re.match('([a-zA-Z0-9]+={0,2})<\/data>', line, flags=re.IGNORECASE)
-    if tag_match:
-      # This line is a tag
-      # Decompose data from tag
-      type_hex, code_hex, length = tag_match.groups()
-      type = type_hex.decode('hex')
-      code = code_hex.decode('hex')
-      # print "type:%s code:%s length:%s" % (type, code, length)
-
-
-
-      if code == 'mdst':
-        # Metadata start
-        metadata_in_progress=True
-      elif code == 'pend':
-        # Play stream end
-        # Do end screen
-        print 'end'
-
-      # if length > 0:
-        # data_mode=True
-        # next(f)
-        # print f.readline()
-
-      # if code == 'asal':
-      #   album = payload
-      # elif code == 'asar':
-      #   artist = payload
-      # elif code == 'ascm':
-      #   comment = payload
-      # elif code == 'asgm':
-      #   genre = payload
-      # elif code == 'minm':
-      #   title = payload
-      # else:
-      #   print 'Unknown code'
-
-      if type == 'ssnc':
-        # print "type:%s code:%s payload:%s" % (type, code, payload)
-        print "type:%s code:%s" % (type, code)
-
+    # print reading_header
+    if reading_header:
+      # This line is a data header
+      if '<data encoding="base64">' in line:
+        reading_header = False
+        reading_data = True
+        continue
       else:
-        # Metadata start
-        metadata_in_progress=False
-
-    elif '<data encoding="base64">' in line:
-      # This line is a data start tag
-      continue
-    elif data_match:
-      # This line is data and a data end tag
-      data = data_match.groups()[0].decode('base64')
-      print data
+        print 'Error: Expected base64 header, got %s' % line
+    elif reading_data and next_data_bucket:
+      # This line is data
+      datum_match = re.match('([a-zA-Z0-9]+={0,2})<\/data>', line, flags=re.IGNORECASE)
+      if datum_match:
+        metadata[next_data_bucket] = datum_match.groups()[0].decode('base64')
+        reading_data = False
+        continue
+      else:
+        print 'Error: Expected data, got %s' % line
     else:
-      print "Tag expected - got %s instead" % line
+      tag_match = re.match('<type>(\w+)<\/type><code>(\w+)<\/code><length>(\d+)<\/length>', line, flags=re.IGNORECASE)
+      if tag_match:
+        # This line is a tag
+        type_hex, code_hex, length_string = tag_match.groups()
+        type = type_hex.decode('hex')
+        code = code_hex.decode('hex')
+        length = int(length_string)
+
+        # if code == 'mdst'
+        # elif code == 'mden'
+        if code == 'mden':
+          print metadata
+          metadata = {}
+
+        if length > 0:
+          next_data_bucket = code
+          reading_header = True
+          continue
+        # elif length 0
+      else:
+        # This line is malformed
+        print 'Error: Expected tag, got %s' % line
