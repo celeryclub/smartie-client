266fc57e5a2597a4f63c85857cf39dfc0588267f
Stephen Davis <stephendavis89@gmail.com>
2015-04-05 19:14:30 -0400

    Handle KeyboardInterrupt

--- ssnc-meta.py
+++ ssnc-meta.py
@@ -3,7 +3,7 @@
 # Usage:
 # python ssnc-meta.py -f '%title\n%artist\n%album\n' test/meta.xml
 
-import argparse, re
+import sys, argparse, re
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--format', required=True)
@@ -22,87 +22,90 @@ def debug(message, level=1):
     print '[DEBUG] %s' % message
   return
 
-fifo = open(args.fifo, 'r')
-with fifo as f:
-  buffer = ''
-  while True:
-    line = f.readline()
-    if line == '':
-        debug('Reached EOF', 2)
-        break
+try:
+  fifo = open(args.fifo, 'r')
+  with fifo as f:
+    buffer = ''
+    while True:
+      line = f.readline()
+      if line == '':
+          debug('Reached EOF', 2)
+          break
 
-    if reading_header:
-      debug('This line is a data header', 2)
+      if reading_header:
+        debug('This line is a data header', 2)
 
-      if not '<data encoding="base64">' in line:
-        print 'Error: Expected base64 header, got "%s"' % line
+        if not '<data encoding="base64">' in line:
+          print 'Error: Expected base64 header, got "%s"' % line
 
-      reading_header = False
-      reading_data = True
-      continue
+        reading_header = False
+        reading_data = True
+        continue
 
-    elif reading_data:
-      debug('This line is data', 2)
+      elif reading_data:
+        debug('This line is data', 2)
 
-      if next_data_bucket:
-        datum_match = re.match('([a-zA-Z0-9+]+={0,2})<\/data>', line, flags=re.IGNORECASE)
-        if datum_match:
-          datum = datum_match.groups()[0].decode('base64')
-          metadata[next_data_bucket] = datum
-          debug('Stored "%s" as "%s"' % (datum, next_data_bucket))
+        if next_data_bucket:
+          datum_match = re.match('([a-zA-Z0-9+]+={0,2})<\/data>', line, flags=re.IGNORECASE)
+          if datum_match:
+            datum = datum_match.groups()[0].decode('base64')
+            metadata[next_data_bucket] = datum
+            debug('Stored "%s" as "%s"' % (datum, next_data_bucket))
+          else:
+            print 'Error: Expected data, got "%s"' % line
         else:
-          print 'Error: Expected data, got "%s"' % line
+          debug('Dropped data on the floor', 2)
+
+        reading_data = False
+        continue
+
       else:
-        debug('Dropped data on the floor', 2)
+        tag_match = re.match('<type>(\w+)<\/type><code>(\w+)<\/code><length>(\d+)<\/length>', line, flags=re.IGNORECASE)
+        if tag_match:
+          debug('This line is a tag', 2)
+          type_hex, code_hex, length_string = tag_match.groups()
+          type = type_hex.decode('hex')
+          code = code_hex.decode('hex')
+          length = int(length_string)
 
-      reading_data = False
-      continue
+          # if code == 'mdst':
+          #   # Metadata start
+          #   # Sanity check - this shouldn't normally be necessary
+          #   debug('Clearing metadata (start)')
+          #   metadata = {}
+          # elif code == 'mden':
+          if code == 'mden':
+            # Metadata end
+            # Magic from http://stackoverflow.com/a/6117124/821471
+            replace = dict((re.escape('%' + k), v) for k, v in metadata.iteritems())
+            pattern = re.compile('|'.join(replace.keys()))
+            formatted = pattern.sub(lambda m: replace[re.escape(m.group(0))], args.format)
+            print formatted.decode('string_escape')
 
-    else:
-      tag_match = re.match('<type>(\w+)<\/type><code>(\w+)<\/code><length>(\d+)<\/length>', line, flags=re.IGNORECASE)
-      if tag_match:
-        debug('This line is a tag', 2)
-        type_hex, code_hex, length_string = tag_match.groups()
-        type = type_hex.decode('hex')
-        code = code_hex.decode('hex')
-        length = int(length_string)
+            metadata = {}
+            debug('Cleared metadata')
 
-        # if code == 'mdst':
-        #   # Metadata start
-        #   # Sanity check - this shouldn't normally be necessary
-        #   debug('Clearing metadata (start)')
-        #   metadata = {}
-        # elif code == 'mden':
-        if code == 'mden':
-          # Metadata end
-          # Magic from http://stackoverflow.com/a/6117124/821471
-          replace = dict((re.escape('%' + k), v) for k, v in metadata.iteritems())
-          pattern = re.compile('|'.join(replace.keys()))
-          formatted = pattern.sub(lambda m: replace[re.escape(m.group(0))], args.format)
-          print formatted.decode('string_escape')
+          if length > 0:
+            bucket = None
 
-          metadata = {}
-          debug('Cleared metadata')
+            if code == 'asal':
+              bucket = 'album'
+            elif code == 'asar':
+              bucket = 'artist'
+            elif code == 'ascm':
+              bucket = 'comment'
+            elif code == 'asgm':
+              bucket = 'genre'
+            elif code == 'ascp':
+              bucket = 'composer'
+            elif code == 'minm':
+              bucket = 'title'
 
-        if length > 0:
-          bucket = None
-
-          if code == 'asal':
-            bucket = 'album'
-          elif code == 'asar':
-            bucket = 'artist'
-          elif code == 'ascm':
-            bucket = 'comment'
-          elif code == 'asgm':
-            bucket = 'genre'
-          elif code == 'ascp':
-            bucket = 'composer'
-          elif code == 'minm':
-            bucket = 'title'
-
-          next_data_bucket = bucket
-          reading_header = True
-          continue
-        # elif length 0
-      else:
-        print 'Error: Expected tag, got "%s"' % line
+            next_data_bucket = bucket
+            reading_header = True
+            continue
+          # elif length 0
+        else:
+          print 'Error: Expected tag, got "%s"' % line
+except KeyboardInterrupt:
+  sys.stdout.flush()
