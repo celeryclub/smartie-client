a473fdb290372e0643081ea79dc8d7b7475d7d24
Steve Davis <stephendavis89@gmail.com>
2015-05-25 18:42:54 -0400

    Create basic LCD client

---
+++ client.py
@@ -0,0 +1,25 @@
+import argparse, sys, socket
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-h', '--host')
+parser.add_argument('-p', '--port', type=int)
+args = parser.parse_args()
+
+HOST = args.host or '10.0.1.5'
+PORT = args.port or 8089
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+client.connect((HOST, PORT))
+
+reply = client.recv(1024).decode()
+print(reply)
+
+def send(message):
+  print('sending ' + message + ' to server...')
+  client.send(message.encode())
+  reply = client.recv(1024).decode()
+  print(reply)
+
+  # while True:
+    # client.recv(1024)
