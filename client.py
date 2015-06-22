912eecab7e61003d8da077c2b4c5c52367a795b7
Steve Davis <steve@celery.club>
2015-06-21 21:36:26 -0400

    Use server hostname instead of IP address

--- client.py
+++ client.py
@@ -1,11 +1,12 @@
 import argparse, sys, socket
+# from ssnc import *
 
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('-h', '--host')
 parser.add_argument('-p', '--port', type=int)
 args = parser.parse_args()
 
-HOST = args.host or '10.0.1.5'
+HOST = args.host or 'orange.local'
 PORT = args.port or 8089
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
