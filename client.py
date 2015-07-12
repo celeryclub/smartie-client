476512596fe7082d58debfa7048aabf9c29004d1
Steve Davis <steve@celery.club>
2015-07-12 18:31:18 -0400

    Run ssnc module from client and send metadata to server

--- client.py
+++ client.py
@@ -1,26 +1,53 @@
-import argparse, sys, socket
-# from ssnc import *
+import argparse, sys, socket, json
+from ssnc import watch
 
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('-h', '--host')
 parser.add_argument('-p', '--port', type=int)
+# parser.add_argument('-v', '--verbose', action='count')
+parser.add_argument('fifo')
 args = parser.parse_args()
 
-HOST = args.host or 'orange.local'
+HOST = args.host or '10.0.1.4'
 PORT = args.port or 8089
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
 client.connect((HOST, PORT))
-
 reply = client.recv(1024).decode()
 print(reply)
 
-def send(message):
-  print('sending ' + message + ' to server...')
-  client.send(message.encode())
+buf = {}
+
+def set_line(data, line):
+  global buf
+  buf['line' + str(line)] = data
+
+def flush():
+  global buf
+  print(repr('flushing'))
+  data = json.dumps(buf)
+  buf = {}
+  print(repr(data))
+  client.send(data.encode())
   reply = client.recv(1024).decode()
   print(reply)
 
-  # while True:
-    # client.recv(1024)
+def clear_screen():
+  global buf
+  buf['line1'] = 'clear'
+  buf['line2'] = 'clear'
+  buf['line3'] = 'clear'
+  buf['line4'] = 'clear'
+  flush()
+
+def debug_log(message, level=1):
+  # if args and args.verbose and args.verbose >= level:
+    # print('[DEBUG] %s' % message)
+  # return
+  print('[DEBUG] %s' % message)
+
+try:
+  watch(args.fifo, set_line, flush, clear_screen, debug_log)
+except KeyboardInterrupt:
+  sys.stdout.flush()
